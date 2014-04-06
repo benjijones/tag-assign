7fe0656d0444ff5185c6d8ba3eedd3d7440f9edb
Benji Jones <benji.s.jones@gmail.com>
2014-04-06 17:27:45 -0400

    add basic KVStore code

--- src/database/DatabaseServer.scala
+++ src/database/DatabaseServer.scala
@@ -1,9 +1,14 @@
 package database
 
+import oracle.kv._
+
 object DatabaseServer {
 
 	def main(args: Array[String]) {
-		KVStoreFactory
+		val config = new KVStoreConfig("tagstore", "localhost:5000")
+		val kvstore = KVStoreFactory.getStore(config)
+		val stats = kvstore.getStats(false)
+		println(stats)
 	}
 
 }
